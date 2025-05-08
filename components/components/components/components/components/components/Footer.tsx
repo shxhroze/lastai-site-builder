84ea4825ab6ca93c20a1fd09dda174135a45fbb5
shxhroze <shehroze@lastai.com.au>
2025-05-08 10:51:22 +1000

    Create Footer.tsx

---
+++ components/components/components/components/components/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center text-gray-500 py-6 border-t border-gray-800 mt-10">
+      <p>© 2025 LastAI. All rights reserved.</p>
+    </footer>
+  );
+}
